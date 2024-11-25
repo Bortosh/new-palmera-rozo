33286de07e6c25cbf017fdc2b9ab9a9276ff3996
RogelioRamir <rogelio.ramirez@inchcape.com>
2024-11-25 15:08:59 -0500

    arreglando detalles

--- src/components/sidebar/components/SidebarList.tsx
+++ src/components/sidebar/components/SidebarList.tsx
@@ -1,20 +1,28 @@
 import navItems from '../routes/routes-list'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import styles from '../styles/sidebarstyle.module.css'
 
 interface SidebarListProps {
     handleClose?: () => void;
 }
 
-const SidebarList = ({handleClose}:SidebarListProps) => {
+const SidebarList = ({ handleClose }: SidebarListProps) => {
+
+    const location = useLocation()
+    const pathNameLocation = location.pathname
 
     return (
         <>
-            {navItems.map(({id, path, className, text}) => (
-                <li key={id} onClick={handleClose} className={styles.wrapperSidebarItems}>
-                    <Link to={path} className={className}>{text}</Link>
-                </li>
-            ))}
+            {navItems.map(({ id, path, text }) => {
+
+                const isActive = path === pathNameLocation;
+
+                return (
+                    <li key={id} onClick={handleClose} className={styles.wrapperSidebarItems}>
+                        <Link to={path} className={`${styles.sidebarItem} ${isActive ? styles.active : ""}`}>{text}</Link>
+                    </li>
+                )
+            })}
         </>
     )
 }
