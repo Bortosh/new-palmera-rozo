3a27b18ab41f1cc60f4d452ae3d070580bea391b
RogelioRamir <rogelio.ramirez@inchcape.com>
2024-11-26 10:22:58 -0500

    fixing details

--- src/sections/ubicacion/UbicacionContainer.tsx
+++ src/sections/ubicacion/UbicacionContainer.tsx
@@ -4,7 +4,7 @@ import styles from './styles/ubicacion.module.css'
 
 const UbicacionContainer = () => {
 
-  const coords = "https://maps.google.com/maps?q=calle%2010%2019%20la%20torre%20rozo+(Mi%20nombre%20de%20egocios)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;?hl=en=&output=embed";
+  const coords = "https://maps.google.com/maps?q=calle%2010%2019%20la%20torre%20rozo+(Mi%20nombre%20de%20negocios)&t=&z=15&ie=UTF8&iwloc=B&hl=en&output=embed&wmode=transparent";
 
   const coordsFooter = 'https://www.google.com/maps?ll=3.619018,-76.421998&z=15&t=m&hl=en&gl=US&mapclient=embed&q=calle+10+19+la+torre+rozo';
 
