6ad11b3970981d03c40fe7205380a681205180cd
RogelioRamir <rogelio.ramirez@inchcape.com>
2024-12-11 12:13:42 -0500

    añadiendo efecto y video

--- src/generalPalmeras/DefaultMain.tsx
+++ src/generalPalmeras/DefaultMain.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material"
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import './styles.css'
 
 interface Props {
@@ -7,15 +7,20 @@ interface Props {
 }
 
 const DefaultMain: FC<Props> = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true)
+  }, []);
 
   return (
     <Box
-      component='main'
-      className={'main-content-layout'}
+      component="main"
+      className={`main-content-layout ${isVisible ? 'fade-in' : ''}`}
     >
       {children}
     </Box>
-  )
-}
+  );
+};
 
-export default DefaultMain
+export default DefaultMain;
