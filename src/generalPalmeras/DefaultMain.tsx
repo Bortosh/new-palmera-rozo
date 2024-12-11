3d6fe5c4e6a687859ca391e3233230ce9e502f12
RogelioRamir <rogelio.ramirez@inchcape.com>
2024-12-11 12:29:58 -0500

    quitando efecto fade in

--- src/generalPalmeras/DefaultMain.tsx
+++ src/generalPalmeras/DefaultMain.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material"
-import { FC, useEffect, useState } from "react"
+import { FC } from "react"
 import './styles.css'
 
 interface Props {
@@ -7,16 +7,11 @@ interface Props {
 }
 
 const DefaultMain: FC<Props> = ({ children }) => {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    setIsVisible(true)
-  }, []);
 
   return (
     <Box
       component="main"
-      className={`main-content-layout ${isVisible ? 'fade-in' : ''}`}
+      className={`main-content-layout`}
     >
       {children}
     </Box>
