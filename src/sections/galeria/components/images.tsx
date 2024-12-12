84c4bba6c8e6a9df40271a98c070b50f40db4f46
RogelioRamir <rogelio.ramirez@inchcape.com>
2024-12-12 10:56:51 -0500

    actualizando data en galeria

--- src/sections/galeria/components/images.tsx
+++ src/sections/galeria/components/images.tsx
@@ -17,27 +17,31 @@ import cocina2 from '../../../assets/photos/cocina_nuevo_2.jpeg'
 import cocina3 from '../../../assets/photos/cocina_nuevo_3.jpeg'
 import cuartoLiteras from '../../../assets/photos/cuarto-literas.jpeg'
 import salonFiesta from '../../../assets/photos/salon-fiesta.jpeg'
+import fincaEvento1 from '../../../assets/photos/finca_evento_1.jpeg'
+import fincaEvento2 from '../../../assets/photos/finca_evento_2.jpeg'
 
 const pictures = [
     { id: 1, src: piscina, alt: 'piscina'},
     { id: 2, src: piscina2, alt: 'piscina2'},
     { id: 3, src: piscina3, alt: 'piscina3'},
-    { id: 1, src: piscina4, alt: 'piscina4'},
-    { id: 2, src: piscina5, alt: 'piscina5'},
-    { id: 3, src: piscina6, alt: 'piscina6'},
+    { id: 4, src: piscina4, alt: 'piscina4'},
+    { id: 5, src: piscina5, alt: 'piscina5'},
+    { id: 6, src: piscina6, alt: 'piscina6'},
     { id: 7, src: piscina7, alt: 'piscina7'},
-    { id: 4, src: fincapalmerasfoto1, alt: 'fincapalmerasfoto1'},
-    { id: 5, src: fincapalmerasfoto2, alt: 'fincapalmerasfoto2'},
-    { id: 6, src: fincapalmerasfoto4, alt: 'fincapalmerasfoto4'},
-    { id: 6, src: fincapalmerasfoto5, alt: 'fincapalmerasfoto5'},
-    { id: 9, src: salonFiesta, alt: 'salon fiesta'},
-    { id: 8, src: cuartoLiteras, alt: 'cuartoLiteras'},
-    { id: 6, src: fincapalmerasfoto8, alt: 'fincapalmerasfoto8'},
-    { id: 6, src: fincapalmerasfoto6, alt: 'fincapalmerasfoto6'},
-    { id: 6, src: fincapalmerasfoto7, alt: 'fincapalmerasfoto7'},
-    { id: 6, src: cocina2, alt: 'cocina2'},
-    { id: 6, src: cocina1, alt: 'cocina1'},
-    { id: 6, src: cocina3, alt: 'cocina3'},
+    { id: 8, src: fincaEvento1, alt: 'finca evento 1'},
+    { id: 9, src: fincaEvento2, alt: 'finca evento 1'},
+    { id: 10, src: fincapalmerasfoto1, alt: 'fincapalmerasfoto1'},
+    { id: 11, src: fincapalmerasfoto2, alt: 'fincapalmerasfoto2'},
+    { id: 12, src: fincapalmerasfoto4, alt: 'fincapalmerasfoto4'},
+    { id: 13, src: fincapalmerasfoto5, alt: 'fincapalmerasfoto5'},
+    { id: 14, src: salonFiesta, alt: 'salon fiesta'},
+    { id: 15, src: cuartoLiteras, alt: 'cuartoLiteras'},
+    { id: 16, src: fincapalmerasfoto8, alt: 'fincapalmerasfoto8'},
+    { id: 17, src: fincapalmerasfoto6, alt: 'fincapalmerasfoto6'},
+    { id: 18, src: fincapalmerasfoto7, alt: 'fincapalmerasfoto7'},
+    { id: 19, src: cocina2, alt: 'cocina2'},
+    { id: 20, src: cocina1, alt: 'cocina1'},
+    { id: 21, src: cocina3, alt: 'cocina3'},
   ];
   
   export default pictures;
